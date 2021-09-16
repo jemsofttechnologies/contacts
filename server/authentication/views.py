b6086c0b95fb0ff1ade9317bc2043b884349052d
jemsofttechnologies <jemsofttechnologies@gmail.com>
2021-09-16 13:32:08 +0300

    server

---
+++ server/authentication/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+
+# Create your views here.
+
+
+class RegisterView(GenericAPIView):
+    def post(self, request):
+        pass
